7111929ed6a6419b009f089e144e47f4ecf096e5
Tomasz <fruboes@cern.ch>
2014-08-25 16:02:16 +0200

    sync

--- GeneratorInterface/RivetInterface/src/CMS_TMF_14_123.cc
+++ GeneratorInterface/RivetInterface/src/CMS_TMF_14_123.cc
@@ -31,6 +31,7 @@ namespace Rivet {
         addProjection(fs, "FS");
         addProjection(FastJets(fs, FastJets::ANTIKT, 0.5),"Jets");
       
+      _hTotal  = bookHistogram1D("hTotal", 1, -0.5, 0.5);
       _hHasJet3  = bookHistogram1D("hasJet3", 1, -0.5, 0.5);
       _hHasJet4  = bookHistogram1D("hasJet4", 1, -0.5, 0.5);
       _hHasJet8  = bookHistogram1D("hasJet8", 1, -0.5, 0.5);
@@ -38,7 +39,9 @@ namespace Rivet {
       _hHasDJet30  = bookHistogram1D("hasDJet30", 1, -0.5, 0.5);
       _hHasDJet40  = bookHistogram1D("hasDJet40", 1, -0.5, 0.5);
       _hHasDJet80  = bookHistogram1D("hasDJet80", 1, -0.5, 0.5);
+      _hHasDJet80ProbeEta = bookHistogram1D("hasDJet80ProbeEta", 23, 2.79, 5.01);
       _hHasDJet140  = bookHistogram1D("hasDJet140", 1, -0.5, 0.5);
+      _hHasDJet140ProbeEta  = bookHistogram1D("hasDJet140ProbeEta", 23,  2.79, 5.01);
 
     } 
 
@@ -46,26 +49,43 @@ namespace Rivet {
 
     void analyze(const Event& event) override {
       const double weight = event.weight();
+      _hTotal->fill(0, weight);
       //Obtain the jets.
       float hardestJet = 0;
       float hardestTag = -1;
       float hardestProbe = -1;
-      foreach (const Jet& j, applyProjection<FastJets>(event, "Jets").jetsByPt(3.0*GeV, -5, 5, RAPIDITY)) {
-            const double jpt = j.momentum().pT();
-            if (jpt > hardestJet) hardestJet = jpt;
+      float hardestProbeEta = -1;
+      //#foreach (const Jet& j, applyProjection<FastJets>(event, "Jets").jetsByPt(3.0*GeV, -5, 5, RAPIDITY)) {
+      foreach (const Jet& j, applyProjection<FastJets>(event, "Jets").jetsByPt(3.0*GeV)) {
             const double eta = std::abs(j.momentum().eta());
+            if (eta > 5) continue;
+            const double jpt = j.momentum().pT();
+            //std::cout << "XXX " << jpt << std::endl;
+            if (jpt > hardestJet) hardestJet = jpt;
             if (eta< 1.4 && jpt > hardestTag) hardestTag = jpt;
-            if (eta > 2.8 && eta <  5 && jpt > hardestProbe) hardestProbe = jpt;
+            if (eta > 2.8 && eta <  5 && jpt > hardestProbe) {
+                hardestProbe    = jpt;
+                hardestProbeEta = eta;
+            }
       }
       if (hardestTag >0 && hardestProbe >0){
         float ptAve = (hardestTag+hardestProbe)/2;
         if (ptAve > 30) _hHasDJet30->fill(0, weight);
         if (ptAve > 40) _hHasDJet40->fill(0, weight);
-        if (ptAve > 80) _hHasDJet80->fill(0, weight);
-        if (ptAve > 140) _hHasDJet140->fill(0, weight);
+        if (ptAve > 80) {
+                _hHasDJet80->fill(0, weight);
+                _hHasDJet80ProbeEta->fill(hardestProbeEta, weight);
+        }
+        if (ptAve > 140){
+                _hHasDJet140->fill(0, weight);
+                _hHasDJet140ProbeEta->fill(hardestProbeEta, weight);
+        }
       }
 
-      if (hardestJet>3) _hHasJet3->fill(0, weight);
+      if (hardestJet>3) {
+            //std::cout << "Jet3 " << weight << std::endl;
+            _hHasJet3->fill(0, weight);
+      }
       if (hardestJet>4) _hHasJet4->fill(0, weight);
       if (hardestJet>8) _hHasJet8->fill(0, weight);
       if (hardestJet>14) _hHasJet14->fill(0, weight);
@@ -75,14 +95,20 @@ namespace Rivet {
     
     /// Normalise histograms etc., after the run
     void finalize() override {
-        scale(_hHasJet3, crossSection()/sumOfWeights()); //# norm to cross section
-        scale(_hHasJet4, crossSection()/sumOfWeights()); //# norm to cross section
-        scale(_hHasJet8, crossSection()/sumOfWeights()); //# norm to cross section
-        scale(_hHasJet14, crossSection()/sumOfWeights()); //# norm to cross section
+
+        double minbias = 7.830E+10;
+        std::cout << "FinaL: " << crossSection() << " " << sumOfWeights() << std::endl;
+        scale(_hTotal, crossSection()/sumOfWeights()/minbias); //# norm to cross section
+        scale(_hHasJet3, crossSection()/sumOfWeights()/minbias); //# norm to cross section
+        scale(_hHasJet4, crossSection()/sumOfWeights()/minbias); //# norm to cross section
+        scale(_hHasJet8, crossSection()/sumOfWeights()/minbias); //# norm to cross section
+        scale(_hHasJet14, crossSection()/sumOfWeights()/minbias); //# norm to cross section
         scale(_hHasDJet30, crossSection()/sumOfWeights()); //# norm to cross section
         scale(_hHasDJet40, crossSection()/sumOfWeights()); //# norm to cross section
         scale(_hHasDJet80, crossSection()/sumOfWeights()); //# norm to cross section
         scale(_hHasDJet140, crossSection()/sumOfWeights()); //# norm to cross section
+        scale(_hHasDJet80ProbeEta, crossSection()/sumOfWeights()); //# norm to cross section
+        scale(_hHasDJet140ProbeEta, crossSection()/sumOfWeights()); //# norm to cross section
 
 
 
@@ -90,6 +116,7 @@ namespace Rivet {
 
   private:
 
+    AIDA::IHistogram1D*  _hTotal;
     AIDA::IHistogram1D*  _hHasJet3;
     AIDA::IHistogram1D*  _hHasJet4;
     AIDA::IHistogram1D*  _hHasJet8;
@@ -98,6 +125,8 @@ namespace Rivet {
     AIDA::IHistogram1D* _hHasDJet40;
     AIDA::IHistogram1D* _hHasDJet80;
     AIDA::IHistogram1D* _hHasDJet140;
+    AIDA::IHistogram1D*  _hHasDJet80ProbeEta;
+    AIDA::IHistogram1D*  _hHasDJet140ProbeEta;
   };
   
   AnalysisBuilder<CMS_TMF_14_123> plugin_CMS_TMF_14_123;
--- GeneratorInterface/RivetInterface/test/runTMF.py
+++ GeneratorInterface/RivetInterface/test/runTMF.py
@@ -12,7 +12,7 @@ process.load("Configuration/Generator/MinBias_13TeV_cfi")
 
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10000)
+    input = cms.untracked.int32(10000*5)
 )
 
 process.source = cms.Source("EmptySource")
@@ -22,6 +22,27 @@ process.load("GeneratorInterface.RivetInterface.rivetAnalyzer_cfi")
 
 process.rivetAnalyzer.AnalysisNames = cms.vstring('CMS_TMF_14_123')
 
+
+# 7.830D+01
+# mb 1e-27
+# pb 1e-36
+
+# minbias:
+#process.rivetAnalyzer.CrossSection = cms.double(7.830E+01*1e9) # 
+# 15...1500
+#process.rivetAnalyzer.CrossSection = cms.double(2.084E+00*1e9) # 
+# 30...1500
+process.rivetAnalyzer.CrossSection = cms.double(1.677E-01*1e9) # 
+
+
 process.p = cms.Path(process.generator*process.rivetAnalyzer)
 
 
+process.generator.PythiaParameters.processParameters = cms.vstring(
+                'MSEL = 1 ! QCD hight pT processes',
+                'CKIN(3) = 30 ! minimum pt hat for hard interactions',
+                'CKIN(4) = 1500 ! maximum pt hat for hard interactions',
+        )      
+
+
+print process.generator.PythiaParameters.processParameters 
